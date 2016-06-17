24bf2da0f88ef243742d0d01e00a10302e53c04c
akravchenko <akravchenko@cogniance.com>
2016-06-17 14:40:22 +0300

    Simply play video

--- StreamVideoCaching/ViewController.swift
+++ StreamVideoCaching/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVPlayer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let playerItem = AVPlayerItem(URL: NSURL(string: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")!)
+        self.player = AVPlayer(playerItem: playerItem)
+
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        self.addChildViewController(playerController)
+        self.view.addSubview(playerController.view)
+        playerController.view.frame = self.view.frame
+        
+        player.play()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
-
-
 }
 
